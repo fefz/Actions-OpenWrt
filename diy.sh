5a5c0f59317400f42be420fc74d8893dfc27c211
swing <imswing@gmail.com>
2020-05-15 10:05:44 +0800

    use hello world

--- diy.sh
+++ diy.sh
@@ -7,6 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.111/g' package/base-files/files/bin/config_generate
+sed -i 's/\#src-git helloworld/\src-git helloworld/g' ./feeds.conf.default
 mkdir temp && cd temp && git clone https://github.com/rosywrt/luci-theme-rosy && cp -R luci-theme-rosy/luci-theme-rosy/ ../package/ && cd .. && rm -rf temp
 mkdir temp && cd temp && git clone https://github.com/vernesong/OpenClash && cp -R OpenClash/luci-app-openclash/ ../package/ && cd .. && rm -rf temp
 

9e4792f412708711f6f797d3e5689452afa308a6
swing <imswing@gmail.com>
2020-08-25 20:34:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 mkdir temp && cd temp && git clone https://github.com/rosywrt/luci-theme-rosy && cp -R luci-theme-rosy/luci-theme-rosy/ ../package/ && cd .. && rm -rf temp
 mkdir temp && cd temp && git clone -b master https://github.com/vernesong/OpenClash && cp -R OpenClash/luci-app-openclash/ ../package/ && cd .. && rm -rf temp
+mkdir temp && cd temp && git clone -b master https://github.com/project-openwrt/openwrt-naiveproxy && cp -R openwrt-naiveproxy/* ../package/ && cd .. && rm -rf temp

77fa1c333abd6539b6d2730a8be11379aac3cb8e
swing <imswing@gmail.com>
2020-08-29 09:27:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,11 +19,6 @@ git clone https://github.com/vernesong/OpenClash && mv OpenClash/luci-app-opencl
 
 # add lienol package
 sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-package.git;master' feeds.conf.default
-sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci.git;dev-17.01' feeds.conf.default
-sed -i '$a src-git packages https://github.com/Lienol/openwrt-packages.git;dev-19.07' feeds.conf.default
-sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git;openwrt-19.07' feeds.conf.default
-sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-19.07' feeds.conf.default
-
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
